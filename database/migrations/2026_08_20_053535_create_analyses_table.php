4559957a927aa591b358701f2be3f4acbea4f67d
wizard-86 <informatikamalam1@gmail.com>
2026-08-22 22:23:04 +0700

    fix bug table analisis

--- database/migrations/2026_08_20_053535_create_analyses_table.php
+++ database/migrations/2026_08_20_053535_create_analyses_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        Schema::create('user_analyses', function (Blueprint $table) {
+        Schema::create('analyses', function (Blueprint $table) {
             $table->id();
             $table->string('incident_code');
             $table->string('video_name');
@@ -20,6 +20,6 @@ return new class extends Migration {
     }
 
     public function down(): void {
-        Schema::dropIfExists('user_analyses');
+        Schema::dropIfExists('analyses');
     }
 };
